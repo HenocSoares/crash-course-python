bd37d168b08e5b797c16dc5d2dca54460d684b75
Henoc Soares <henocsoares@hotmail.com.br>
2024-07-07 20:35:54 -0300

    Update guessingGame.py

--- guessingGame.py
+++ guessingGame.py
@@ -2,7 +2,7 @@ import sys
 guessn = 33
 userNum = 0
 while userNum < guessn or userNum > guessn:
-    userNum = int(input("Qual é o número? "))
+    userNum = int(input("What is your guess?"))
     if userNum == guessn:
         print("You're right! Your guess is correct! Congrats!")
         sys.exit()
